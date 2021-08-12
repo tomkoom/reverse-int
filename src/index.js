97a878a443860d4def5d6a8d7185c10745a9c9c4
tomkoom <tomash.sugint@gmail.com>
2021-08-12 16:48:46 +0300

    write reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let nArr = Math.abs(n).toString().split('');
+    let res = [];
+
+    for (let i = 0; i < n.toString().length; i++) {
+        res.push(nArr.pop());
+    }
+
+    return +res.join('');
 }
+
